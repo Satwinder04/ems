5cfb17edaa6401d446095c614e949e25c6447c54
Satwinder04 <satwindersalan@gmail.com>
2024-10-28 09:14:48 +0530

    Day 3

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "../others/Header";
+import CreateTask from "../others/CreateTask";
+import AllTask from "../others/AllTask";
 
 const AdminDashboard = () => {
   return (
@@ -7,57 +9,8 @@ const AdminDashboard = () => {
       <div className="px-10 py-5">
         <Header />
 
-        <div>
-          <form action="" className=" flex justify-between items-start gap-5">
-            <div className="w-1/2">
-              <div>
-                <h3 className="text-xl">Task title</h3>
-                <input
-                  className="p-3 border-2 w-full border-black"
-                  type="text"
-                  placeholder="make a ui design"
-                />
-              </div>
-
-              <div>
-                <h3 className="text-xl">date</h3>
-                <input
-                  type="date"
-                  className="p-3 border-2 w-full border-black"
-                />
-              </div>
-              <div>
-                <h3 className="text-xl">Asign To</h3>
-                <input
-                  type="text"
-                  placeholder="Employee Name"
-                  className="p-3 border-2 w-full border-black"
-                />
-              </div>
-              <div>
-                <h3 className="text-xl">Category</h3>
-                <input
-                  type="text"
-                  placeholder="design, dev, etc"
-                  className="p-3 border-2 w-full border-black"
-                />
-              </div>
-            </div>
-            <div className="w-1/2">
-              <h3 className="text-xl">Description</h3>
-              <textarea
-                name=""
-                id=""
-                cols="30"
-                rows="11"
-                className="p-3 border-2 w-full border-black"
-              ></textarea>
-              <button className="text-xl bg-red-400 w-full py-3 text-white uppercase">
-                Create Task
-              </button>
-            </div>
-          </form>
-        </div>
+        <CreateTask/>
+        <AllTask/>
       </div>
     </>
   );
---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='alltask' className='mt-5 h-40 overflow-auto'>
+        <div className='bg-red-400 py-2 px-4 flex justify-between mt-4 text-white'>
+            <h2>Satwinder</h2>
+            <h3>Make ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 flex justify-between mt-4 text-white'>
+            <h2>Satwinder</h2>
+            <h3>Make ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 flex justify-between mt-4 text-white'>
+            <h2>Satwinder</h2>
+            <h3>Make ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 flex justify-between mt-4 text-white'>
+            <h2>Satwinder</h2>
+            <h3>Make ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 py-2 px-4 flex justify-between mt-4 text-white'>
+            <h2>Satwinder</h2>
+            <h3>Make ui design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
